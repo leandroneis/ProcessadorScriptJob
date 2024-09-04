f68d513242c656af7ab2420a1adbf9291b68c83f
Leandro Gustavo Neis <lgn200@gmail.com>
2024-09-04 17:44:34 -0300

    49. Prática: Processadores de execução de scripts

--- src/main/java/com/springbatch/processadorscript/processor/ProcessadorScriptProcessorConfig.java
+++ src/main/java/com/springbatch/processadorscript/processor/ProcessadorScriptProcessorConfig.java
@@ -1,6 +1,8 @@
 package com.springbatch.processadorscript.processor;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.ScriptItemProcessor;
+import org.springframework.batch.item.support.builder.ScriptItemProcessorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,11 @@ import com.springbatch.processadorscript.dominio.Cliente;
 public class ProcessadorScriptProcessorConfig {
 	@Bean
 	public ItemProcessor<Cliente, Cliente> processadorScriptProcessor() {
-		// TODO: Implementar processador aqui...
-		return cliente -> cliente;
+
+		return new ScriptItemProcessorBuilder<Cliente, Cliente>().language("nashorn")
+		.scriptSource("var email= item.getEmail();" + 
+									"var arquivoExiste = `ls | grep ${email}.txt`;"  +
+									"if (!arquivoExiste) item; else null;"
+									).build();
 	}
 }
